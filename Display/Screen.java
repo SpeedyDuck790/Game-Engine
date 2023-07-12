739a8824c269686e1bfe37c18f9ef16954db9647
james-hill <jamesleehill@outlook.com>
2023-07-12 15:53:49 +1000

    write and load in correct file

--- Display/Screen.java
+++ Display/Screen.java
@@ -1,9 +1,6 @@
 package Display;
 
 import java.awt.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
@@ -88,28 +85,8 @@ public class Screen extends JPanel implements Runnable {
 
     public void update() {
         Updating u = new Updating();
-        System.out.println("File Log");
-        String content = "";
-        try (Scanner reader = new Scanner(new File("PlayerData/save1.txt"))) {
-            while (reader.hasNextLine()) {
-                content += reader.nextLine();
-            }
-            String[] GameData = content.split("#");
-            System.out.println("PlayerData: " + GameData[0]);
-            System.out.println("PlayerData: " + GameData[1]);
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Reading failed");
-        }
-        try (FileWriter writer = new FileWriter("PlayerData/save1.txt")) {
-            writer.write("PlayerData:write#" + i);
-            i++;
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Writing failed");
-        }
+        u.Loading();
+        u.WritingNewLoad();
     }
 
     // thread
--- Display/Updating.java
+++ Display/Updating.java
@@ -1,8 +1,38 @@
 package Display;
 
-public class Updating {
-    public void OItest() {
-        System.out.println("Updating.OItest()");
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Updating {
+
+    public void Loading() {
+        System.out.println("FileLoaded");
+        String content = "";
+        try (Scanner reader = new Scanner(new File("PlayerData/save1.txt"))) {
+            while (reader.hasNextLine()) {
+                content += reader.nextLine();
+            }
+            String[] GameData = content.split("#");
+            for (String data : GameData) {
+                System.out.println(data);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Reading failed");
+
+        }
+    }
+
+    public void WritingNewLoad() {
+        try (FileWriter writer = new FileWriter("PlayerData/save1.txt")) {
+            writer.write("write#");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Writing failed");
+        }
     }
 }
