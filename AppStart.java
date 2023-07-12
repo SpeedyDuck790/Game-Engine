d87db8b129c6a25dcabece1e1733dd79f9dc3cb2
james-hill <jamesleehill@outlook.com>
2023-07-12 10:36:21 +1000

    screen popup checkpoint

--- AppStart.java
+++ AppStart.java
@@ -2,6 +2,7 @@ public class AppStart {
     public static void main(String[] args) {
         System.out.println("AppStart.main()");
         Display.Screen s = new Display.Screen();
+        s.Startpg();
         s.startGthread();
     }
 }
--- Display/Screen.java
+++ Display/Screen.java
@@ -1,11 +1,35 @@
 package Display;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Screen extends JPanel implements Runnable {
 
+    final int ResoScale = 5;
+
+    final int TileSize = 10 * ResoScale; // 10x10
+    final int ScrWidth = TileSize * 80;
+    final int ScrHeight = TileSize * 40;
+
     public Screen() {
-        System.out.println("Screen.Screen()");
+        this.setSize(ScrWidth, ScrHeight);
+        this.setDoubleBuffered(getFocusTraversalKeysEnabled());
+        // this.addKeyListener(keyValue);
+        this.setFocusable(true);
+    }
+
+    public static void Startpg() {
+        JFrame Screen = new JFrame();
+        Screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Screen.setResizable(false);
+        Screen.setTitle("2D Game Engine");
+        Screen Startpg = new Screen();
+        Screen.add(Startpg);
+
+        Screen.pack();
+        Screen.setLocationRelativeTo(null);
+        Screen.setVisible(true);
+
     }
 
     @Override
